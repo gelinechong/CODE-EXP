801b2c2d8e50fea66300147f3e451b578feaeef9
gelinechong <gchong010@e.ntu.edu.sg>
2021-06-14 12:13:59 +0800

    instigate conflict

--- App.js
+++ App.js
@@ -6,9 +6,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>HELLOOO WORLD ! </Text>
+      <Text>HelloWorld!</Text>
       <StatusBar style="auto" />
     </View>
+  
   );
 }
 
