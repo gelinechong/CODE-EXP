9e46813565621a489617a29b868658c202036c47
nicliaw <nicoleliaw@Liaws-MacBook-Pro.local>
2021-06-15 15:00:46 +0800

    Added all three screens and refactored into seperate files

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./Screens/HomeSceen";
+import SettingsScreen from "./Screens/SettingsScreen";
+import ContactsScreen from "./Screens/ContactsScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>change something again!</Text>
-      <StatusBar style="auto" />
-    </View>
-  
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Contacts" component={ContactsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
